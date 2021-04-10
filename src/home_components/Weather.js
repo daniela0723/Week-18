1496a8cf1ab4c3470a9a82b417189922b8813ff4
Daniela Carrillo <74390016+daniela0723@users.noreply.github.com>
2021-04-10 12:19:09 -0700

    Update Weather.js

--- src/home_components/Weather.js
+++ src/home_components/Weather.js
@@ -5,6 +5,7 @@ import "../App.css";
 function Weather() {
   //API Key is called from .env
   //Github will not upload files with an API Key
+  
   const [weather, setWeather] = useState("");
   const [temperature, setTemperature] = useState(0);
   const [cityName, setCityName] = useState("");
@@ -13,7 +14,7 @@ function Weather() {
   const fetchWeather = async () => {
     try {
       const res = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?zip=89030,us&appid=${API_KEY}&units=imperial`
+        `https://api.openweathermap.org/data/2.5/weather?zip=89030,us&appid=${process.env.WEATHER_API_KEY}&units=imperial`
       );
       setTemperature(Math.round(res.data.main.temp));
       setCityName(res.data.name);
